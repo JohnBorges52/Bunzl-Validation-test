159cd1e3987f4123721ebcc4d31c842234888e31
john <jrborges52@gmail.com>
2022-09-27 18:32:51 -0700

    add topnavbar

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { TopNavBar } from './components/TopNavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/TopNavBar.jsx
@@ -0,0 +1,19 @@
+import '../styles/topnav.scss'
+
+
+export const TopNavBar = () => {
+
+  
+  return(
+      <div className="topnav-container">
+        <div className="logo">
+          
+        </div> 
+        <div className='login'>
+        LOGIN
+
+        </div>
+
+      </div>
+  )
+}
