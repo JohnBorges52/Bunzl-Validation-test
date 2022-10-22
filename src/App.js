b92a20745a2550ec679fd89a21ede327ed2ba8f0
john <jrborges52@gmail.com>
2022-10-22 14:00:23 -0700

    add validation if the user is logged in or not

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import { Login } from './components/Login'
 import { Routes, Route, useNavigate } from "react-router-dom";
 
 function App() {
+
+  const user = localStorage.getItem("user");
   return (
     <div className="App">
       <TopNavBar />
       <Routes>
-        <Route path="/" element={<MainContainer />} />
+        <Route path="/" element={user ? <MainContainer /> : <Login />} />
+        <Route path="/backorder" element={<MainContainer />} />
         <Route path="/login" element={<Login />} />
-
       </Routes>
       <BottomNavBar />
 
     </div>
   );
+
 }
 
 export default App;
